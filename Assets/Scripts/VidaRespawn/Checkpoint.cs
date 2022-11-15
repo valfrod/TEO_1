34e3cd4109d33837220e44645a6ccfc4d742f592
JoseJuan777 <113146338+JoseJuan777@users.noreply.github.com>
2022-11-15 01:47:15 -0600

    checkpoints
    checkpoints siempre funcionan

--- Assets/Scripts/VidaRespawn/Checkpoint.cs
+++ Assets/Scripts/VidaRespawn/Checkpoint.cs
@@ -6,23 +6,26 @@ public class Checkpoint : MonoBehaviour
 {
     float PosI = 0f;
     float PosN = 0f;
+    GameObject teon;
 
     private void OnTriggerEnter(Collider other)
     {
+        teon = GameObject.Find("TEO");
         PosI = TeoState.position.x;
         PosN = this.transform.position.x;
+        //print(teon.transform.position);
         if (other.tag == "Player")
         {
             if (this.tag == "hcheck")
             {
-                TeoState.hposition = this.transform.position;
+                TeoState.hposition = teon.transform.position;
                 TeoState.SavePrefs();
                 print("Ultimo checkpoint" + TeoState.hposition);
                 print(PosI + "seg" + PosN);
             }
             else
             {
-                TeoState.position = this.transform.position;
+                TeoState.position = teon.transform.position;
                 TeoState.SavePrefs();
                 print("Ultimo checkpoint_nohcheck" + TeoState.position);
                 print(PosI + "seg" + PosN);
