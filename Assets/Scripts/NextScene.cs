aebc4c878d8d33c621cc90d2928517fdad579f7d
a00824999 <112212623+a00824999@users.noreply.github.com>
2022-11-14 22:53:11 -0600

    Revert "Arreglos"
    This reverts commit eff13002284f557a04c73dc37ed426d814cefe57.

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class NextScene : MonoBehaviour
+public class NextScene1 : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
